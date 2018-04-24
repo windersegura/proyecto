df2ffb009af6c98e6d6577716e7275b93bb33056
windersegura <windersegura@hotmail.com>
2018-04-24 01:16:17 -0600

    nuevos cambios

--- index.php
+++ index.php
@@ -8,27 +8,29 @@
 		<body>
 		<div class="contenedor">
 			<header role="banner">
-				<div class="logo">
-					<img src="img(1)/logoU.jpg" height="300" alt="logo de la universidad">
-				</div>
+					<div class="logo">
+						<img src="img(1)/logoU.jpg" height="300" alt="logo de la universidad">
+						<h2>Universidad Gran Jaguar de Guatemala</h2>
 
-				<nav class="menu" role="navigation">
-					<ul>
-						<li><a href="admin.php">Administrativo</a></li>
-						<li><a href="student.php">Estudiante</a></li>
-						<li><a href="teacher.php">Maestro</a></li>
-					</ul>
-				</nav>
+					</div>
+
+					<nav class="menu" role="navigation">
+						<ul>
+							<li><a href="admin.php">Administrativo</a></li>
+							<li><a href="student.php">Estudiante</a></li>
+							<li><a href="teacher.php">Maestro</a></li>
+						</ul>
+					</nav>
 
 			</header>
-			
+
 
 			<main class="contenedor-principal" role="main">
-				
+
 				<section>
 					<h2> Construimos Un Futuro Juntos</h2>
 						<div>
-							
+
 							<img src="img(1)/universitarios1.jpg" alt="" width="500">
 							<img src="img(1)/profecionales.jpg" alt="" width="500">
 							<img src="img(1)/superacion.jpg" alt="" width="500">
@@ -36,15 +38,15 @@
 
 
 				</section>
-				
+
 			</main>
 
 		</div>
 
 		<footer role="contentinfo">
-			
+
 				<div class="descripcion-nosotros">
-					
+
 
 
 				</div>
--- main_app/admin/newstudent.php
+++ main_app/admin/newstudent.php
@@ -1,19 +1,19 @@
-<?php  
+<?php
 	session_start();
 
 	//si la variable de session existe se queda de lo contrario lo desloguea o lo envia para su usuario correcto
 	if(isset($_SESSION['usuario']))
 	{
-		if ($_SESSION['usuario']['TipoUsuario'] == "Secretario") 
+		if ($_SESSION['usuario']['TipoUsuario'] == "Secretario")
 		{
 			header("Location: ../secretary/");
 		}
 
-		else   if ($_SESSION['usuario']['TipoUsuario'] != "Admin") 		
+		else   if ($_SESSION['usuario']['TipoUsuario'] != "Admin")
 		{
 			header("Location: ../salir.php");
 		}
-		
+
 	}
 	else
 	{
@@ -27,58 +27,74 @@
 	<title>Nuevo Estudiante</title>
 </head>
 	<body>
-		<center>	
-			<form class="form-group" action="studentupdate.php" method="POST" >
-				<label>Nombre Completo <span><em>(requerido)</em></span></label><br> 
-		        <input  pattern="[A-Za-z ]{1,45}" name="nombre" class="form-input form-control" placeholder="Ingrese Nombre Completo" required/><br> 
+		<header>
 
-		        <label>Edad<span><em>(requerido)</em></span></label><br> 
-		        <input  pattern="[0-9]{1,2}" name="edad" class="form-input form-control" placeholder="Ingrese Edad" required/><br> 
+		</header>
 
-		        <label>Direccion <span><em>(requerido)</em></span></label><br> 
-		        <input  pattern="[A-Za-z-_0-9,. ]{1,240}" name="direccion" class="form-input form-control" placeholder="Ingrese Su Direccion De Ubicacion" required/><br>
+		<main id="main_form1">
+			<header>
+					<h1>REGISTRO DE ESTUDIANTES</h1>
+			</header>
+			<section id="seccion_form">
+	
+				<center>
+					<fieldset>
+						<legend>Ingrese sus Datos</legend>
+							<form class="form-group" action="studentupdate.php" method="POST" >
+								<label>Nombre Completo <span><em>(requerido)</em></span></label><br>
+						        <input  pattern="[A-Za-z ]{1,45}" name="nombre" class="form-input form-control" placeholder="Ingrese Nombre Completo" required/><br>
 
-		        <label>Telefono<span><em>(requerido)</em></span></label><br> 
-		        <input  pattern="[0-9]{1,10}" name="telefono" class="form-input form-control" placeholder="Ingrese Numero Telefonico" required/><br>
+						        <label>Edad<span><em>(requerido)</em></span></label><br>
+						        <input  pattern="[0-9]{1,2}" name="edad" class="form-input form-control" placeholder="Ingrese Edad" required/><br>
 
-		        <div class="form-group">
-				<label for="sexo" class="col-sm-2 control-label">Sexo</label>
-					<div class="col-sm-10">
-						<select class="form-control" id="sexo" name="sexo">
-							<option value=""></option>
-							<option value="Masculino">Masculino</option>
-							<option value="Femenino">Femenino</option>
-						</select>
-					</div>
-				</div>
+						        <label>Direccion <span><em>(requerido)</em></span></label><br>
+						        <input  pattern="[A-Za-z-_0-9,. ]{1,240}" name="direccion" class="form-input form-control" placeholder="Ingrese Su Direccion De Ubicacion" required/><br>
 
-				
+						        <label>Telefono<span><em>(requerido)</em></span></label><br>
+						        <input  pattern="[0-9]{1,10}" name="telefono" class="form-input form-control" placeholder="Ingrese Numero Telefonico" required/><br>
+
+						        <div class="form-group">
+								<label for="sexo" class="col-sm-2 control-label">Sexo</label>
+									<div class="col-sm-10">
+										<select class="form-control" id="sexo" name="sexo">
+											<option value=""></option>
+											<option value="Masculino">Masculino</option>
+											<option value="Femenino">Femenino</option>
+										</select>
+									</div>
+								</div>
 
 
-				<label>Numero De DPI<span><em>(requerido)</em></span></label><br> 
-		        <input  pattern="[0-9]{1,13}" name="dpi" class="form-input form-control" placeholder="Su Numero De DPI" required/><br>
 
-		        <label>Contraseña<span><em>(requerido)</em></span></label><br> 
-		        <input  pattern="[0-9]{1,13}" name="password" class="form-input form-control" value="<?php $random_number = intval( "0" . rand(1,9) . rand(0,9) . rand(0,9) . rand(0,9) . rand(0,9) . rand(0,9) );  echo $random_number;?>" required></input><br>
 
-		        <div class="form-group">
-				<label for="tipo" class="col-sm-2 control-label">Tipo</label>
-					<div class="col-sm-10">
-						<select class="form-control" id="tipo" name="tipo">
-							<option value=""></option>
-							<option value="Alumno">Alumno</option>
-							
-						</select>
-					</div>
-				</div>
+								<label>Numero De DPI<span><em>(requerido)</em></span></label><br>
+						        <input  pattern="[0-9]{1,13}" name="dpi" class="form-input form-control" placeholder="Su Numero De DPI" required/><br>
 
-		        <label>Correo Electronico<span><em>(requerido)</em></span></label><br> 
-		        <input type="text" pattern="[0-9A-Za-z_-@.]{1,35}" name="correo" class="form-input form-control" placeholder="Su Correo" required/><br>
-		
-				<input class="btn__submit" type="submit" value="REGISTRAR">
-		        
-				<a href="index.php">Regresar</a>
-			</form>
-		</center>
+						        <label>Contraseña<span><em>(requerido)</em></span></label><br>
+						        <input  pattern="[0-9]{1,13}" name="password" class="form-input form-control" value="<?php $random_number = intval( "0" . rand(1,9) . rand(0,9) . rand(0,9) . rand(0,9) . rand(0,9) . rand(0,9) );  echo $random_number;?>" required></input><br>
+
+						        <div class="form-group">
+								<label for="tipo" class="col-sm-2 control-label">Tipo</label>
+									<div class="col-sm-10">
+										<select class="form-control" id="tipo" name="tipo">
+											<option value=""></option>
+											<option value="Alumno">Alumno</option>
+
+										</select>
+									</div>
+								</div>
+
+						        <label>Correo Electronico<span><em>(requerido)</em></span></label><br>
+						        <input type="text" pattern="[0-9A-Za-z_-@.]{1,35}" name="correo" class="form-input form-control" placeholder="Su Correo" required/><br>
+
+								<input class="btn__submit" type="submit" value="REGISTRAR">
+
+								<a href="index.php">Regresar</a>
+							</form>
+					</fieldset>
+
+				</center>
+			</section>
+		</main>
 	</body>
 </html>
