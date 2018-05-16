4a2a779d544fa9ea64645c92f9f3789805144285
Gerson <38637180+GrsnChml@users.noreply.github.com>
2018-05-16 13:04:17 -0600

    nuevo
    se agregaron las funcionalidades de la secretaria como, consultar alumno y maestro e ingresar alumno y maestro

---
+++ main_app/secretary/consultstudent.php
@@ -0,0 +1,132 @@
+<?php
+	session_start();
+
+	//si la variable de session existe de lo contrario no se hace nada
+	if(isset($_SESSION['usuario']))
+	{
+
+		if($_SESSION['usuario']['TipoUsuario'] == "Admin")
+		{
+			header("Location: ../admin/");
+		}
+		else if ($_SESSION['usuario']['TipoUsuario'] != "Secretario")
+		{
+			header("Location: ../salir.php");
+		}
+	}
+	else
+	{
+		header('Location: ../../');
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Consulta Del Alumno</title>
+	<link rel="stylesheet" href="../../css/bootstrap.css">
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+</head>
+	<body>
+		<div class="pos-f-t">
+	  	<div class="collapse" id="navbarToggleExternalContent">
+		    <div class="bg-primary p-4">
+		      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+			  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				    	<span class="navbar-toggler-icon"></span>
+				  	</button>
+				  	<div class="collapse navbar-collapse " id="navbarTogglerDemo01">
+
+				    	<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+
+
+
+					      	<li class="nav-item dropdown">
+						        <a class="nav-link dropdown-toggle bold" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						          Opciones
+						        </a>
+						        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						          <a class="dropdown-item" href="newstudent.php">Ingresar Alumno</a>
+						          <a class="dropdown-item" href="newteacher.php">Ingresar Maestro</a>
+						          <a class="dropdown-item" href="consultstudent.php">Consultar Alumno</a>
+						          <a class="dropdown-item" href="consultteacher.php">Consultar Maestro</a>
+						          <a class="dropdown-item" href="#">Solvencias (PENDIENTE HASTA TENER PROTOCOLO)</a>
+						          <a class="dropdown-item" href="#">Asignar Alumno a curso</a>
+						          <a class="dropdown-item" href="#">Asignar Maestro a curso</a>
+						        </div>
+					      	</li>
+					      	<li class="nav-item">
+					        	<a class="nav-link " href="../salir.php">Salir</a>
+					      	</li>
+				    	</ul>
+
+				  </div>
+				</nav>
+		    </div>
+		  </div>
+		  <nav class="navbar navbar-dark bg-primary">
+		    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		  </nav>
+		</div>
+		<center><br><h1>LISTADO DE ALUMNOS</h1><br><br>
+
+		<table class="table">
+
+			<th>Nombre</th>
+			<th>Edad</th>
+			<th>Direccion</th>
+			<th>Telefono</th>
+			<th>Sexo</th>
+			<th>DPI</th>
+			<th>Password</th>
+
+			<th>Correo</th>
+			<th><a href="newstudent.php"><button type="button" name="nuevo" class="btn btn-info">Nuevo</Button></a></th>
+			<th><a href="#"><button type="button" name="imprimir" class="btn btn-info">Imprimir Todo</Button></a></th>
+
+			<?php
+			include "../conexion.php";
+			$busqueda = "SELECT * FROM alumnos";
+			$busqueda = $mysqli->query($busqueda) or die (mysql_error($mysqli));
+			while($fila=$busqueda->fetch_assoc())
+			{
+				$idAlumno = mysqli_real_escape_string($mysqli,$fila['idAlumno']);
+				$nombre = mysqli_real_escape_string($mysqli,$fila['Nombre']);
+				$edad = mysqli_real_escape_string($mysqli,$fila['Edad']);
+				$direccion = mysqli_real_escape_string($mysqli,$fila['Direccion']);
+				$telefono = mysqli_real_escape_string($mysqli,$fila['Telefono']);
+				$sexo = mysqli_real_escape_string($mysqli,$fila['Sexo']);
+				$dpi = mysqli_real_escape_string($mysqli,$fila['DPI']);
+				$password = mysqli_real_escape_string($mysqli,$fila['Password']);
+				$tipoUsuario = mysqli_real_escape_string($mysqli,$fila['TipoUsuario']);
+				$correo = mysqli_real_escape_string($mysqli,$fila['Correo']);
+
+				echo "<tr>";
+					$idAlumno;
+					echo "<td><center>"; echo $nombre; echo "</center></td>";
+					echo "<td><center>"; echo $edad; echo "</center></td>";
+					echo "<td><center>"; echo $direccion; echo "</center></td>";
+					echo "<td><center>"; echo $telefono; echo "</center></td>";
+					echo "<td><center>"; echo $sexo; echo "</center></td>";
+					echo "<td><center>"; echo $dpi; echo "</center></td>";
+					echo "<td><center>"; echo $password; echo "</center></td>";
+					 $tipoUsuario;
+					echo "<td><center>"; echo $correo; echo "</td>";
+					
+					echo "<td><a href='#?numero=".$idAlumno."'><button type='button' name='imprimir' class='btn btn-success'>Imprimir</Button></a></td>";
+				echo "<tr>";
+			}
+
+
+			?>
+			</table>
+
+			<a href="index.php">Volver</a>
+		</center>
+		<script src="../../js/bootstrap.js"></script>
+	<script src="../../js/jquery-3.2.1.min.js"></script>
+	<script src="../../js/bootstrap.min.js"></script>
+	</body>
+</html>
--- main_app/secretary/index.php
+++ main_app/secretary/index.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 	session_start();
 
 	//si la variable de session existe de lo contrario no se hace nada
@@ -25,10 +25,65 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Bienvenido Secretario</title>
+	<title>Bienvenido Secretari@</title>
+	<link rel="stylesheet" href="../../css/bootstrap.css">
+	<script src="../../js/jquery-3.3.1.min.js"></script>
+	<script src="../../js/bootstrap.min.js"></script>
 </head>
 	<body>
-		<h1> Bienvenido <?php echo $_SESSION['usuario']['Nombre'] ?></h1>
-		<a href="../salir.php">SALIR</a>	
+		<div class="pos-f-t">
+	  	<div class="collapse" id="navbarToggleExternalContent">
+		    <div class="bg-primary p-4">
+		      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+			  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				    	<span class="navbar-toggler-icon"></span>
+				  	</button>
+				  	<div class="collapse navbar-collapse " id="navbarTogglerDemo01">
+
+				    	<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+
+
+
+					      	<li class="nav-item dropdown">
+						        <a class="nav-link dropdown-toggle bold" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						          Opciones
+						        </a>
+						        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						          <a class="dropdown-item" href="newstudent.php">Ingresar Alumno</a>
+						          <a class="dropdown-item" href="newteacher.php">Ingresar Maestro</a>
+
+						          <a class="dropdown-item" href="consultstudent.php">Consultar Alumno</a>
+						          <a class="dropdown-item" href="consultteacher.php">Consultar Maestro</a>
+						          <a class="dropdown-item" href="#">Solvencias (PENDIENTE HASTA TENER PROTOCOLO)</a>
+						          <a class="dropdown-item" href="assignmentstudent.php">Asignar Alumno a curso</a>
+						          <a class="dropdown-item" href="#">Asignar Maestro a curso</a>
+
+
+
+						        </div>
+					      	</li>
+					      	<li class="nav-item">
+					        	<a class="nav-link " href="../salir.php">Salir</a>
+					      	</li>
+				    	</ul>
+
+				  </div>
+				</nav>
+		    </div>
+		  </div>
+		  <nav class="navbar navbar-dark bg-primary">
+		    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		  </nav>
+		</div>
+		<center>
+			<br><br>
+			<h1> Bienvenido <?php echo $_SESSION['usuario']['Nombre'] ?></h1>
+			<br>
+			<img src="../../img(1)/secretary_icon.png" style="width: 20%">
+		</center>
+
+		<a href="../salir.php">SALIR</a>
 	</body>
 </html>
