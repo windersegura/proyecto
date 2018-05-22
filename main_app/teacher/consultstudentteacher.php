ef40a872db0fc5c32a1255e3459faa17893c8d91
windersegura <windersegura@hotmail.com>
2018-05-21 22:04:51 -0600

    s

--- main_app/teacher/consultstudentteacher.php
+++ main_app/teacher/consultstudentteacher.php
@@ -1,109 +1,5 @@
-
 <?php
-	session_start();
-	//si la variable de session existe de lo contrario no se hace nada
-	if(isset($_SESSION['usuario']))
-	{
-
-		if($_SESSION['usuario']['TipoUsuario'] != "Alumno")
-		{
-			header('Location: ../salir.php');
-		}
-
-	}
-	else
-	{
-		header('Location: ../../');
-	}
-$idAlumno = $_SESSION['usuario']['idAlumno'];
-	$consulta=consultaprod($idAlumno);
-	function consultaprod( $no_prod )
-	{
-		include '../conexion.php';
-		$sentencia="SELECT * FROM alumnos WHERE idAlumno='".$no_prod."' ";
-		$resultado= $mysqli->query($sentencia) or die ("Error al consultar producto".mysqli_error($mysqli));
-		$fila=$resultado->fetch_assoc();
-
-		return
-		[
-			$fila['idAlumno'],
-			$fila['Nombre'],
-			$fila['Edad'],
-			$fila['Direccion'],
-			$fila['Telefono'],
-			$fila['Sexo'],
-			$fila['DPI'],
-			$fila['Password'],
-			$fila['TipoUsuario'],
-			$fila['Correo'],
-		];
-	}
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Bienvenido Estudiante</title>
-	<link rel="stylesheet" href="../../css/bootstrap.css">
-	<script type="text/javascript" src="../../js/jquery-3.3.1.min.js"></script>
-	<script type="text/javascript" src="../../js/bootstrap.min.js"></script>
-</head>
-	<body>
-		<div class="pos-f-t">
-	  	<div class="collapse" id="navbarToggleExternalContent">
-		    <div class="bg-danger p-4">
-		      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-			  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-				    	<span class="navbar-toggler-icon"></span>
-				  	</button>
-				  	<div class="collapse navbar-collapse " id="navbarTogglerDemo01">
-				    	<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-					      	<li class="nav-item dropdown">
-						        <a class="nav-link dropdown-toggle bold" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						          Opciones
-						        </a>
-						        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-						          <a class="dropdown-item" href="#">Consultar Horario</a>
-						          <a class="dropdown-item" href="consultcoursestudent.php">Consultar Curso</a>
-						          <a class="dropdown-item" href="consultteacherstudent.php">Consultar Maestro</a>
-											<a class="dropdown-item" href="formPago.php">Realizar Pago</a>
-						        </div>
-					      	</li>
-					      	<li class="nav-item">
-					        	<a class="nav-link " href="../salir.php">Salir</a>
-					      	</li>
-				    	</ul>
-				  </div>
-				</nav>
-		    </div>
-		  </div>
-		  <nav class="navbar navbar-dark bg-danger">
-		    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-		      <span class="navbar-toggler-icon"></span>
-		    </button>
-		  </nav>
-		</div>
-		<center><br>
-			<?php
-		 	if ($consulta[5] =="Masculino")
-			{
-				 echo "<h1> BIENVENIDO $consulta[1]</h1>" ;
-		 	}
-			else
-			{
-				echo "<h1> BIENVENIDA $consulta[1]</h1>" ;
-			}
-				$query = "SELECT Solvencia ";
-			?>
-
-
-			</aside>
-		</center>
-	</body>
-</html>
-
-
-<?php
+require '../conexion.php';
 	session_start();
 	//si la variable de session existe de lo contrario no se hace nada
 	if(isset($_SESSION['usuario']))
@@ -117,6 +13,8 @@ $idAlumno = $_SESSION['usuario']['idAlumno'];
 	{
 		header('Location: ../../');
 	}
+	$idProfesor = $_SESSION['usuario']['idProfesor'];
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -128,12 +26,77 @@ $idAlumno = $_SESSION['usuario']['idAlumno'];
 	<script type="text/javascript" src="../../js/bootstrap.min.js"></script>
 </head>
 	<body>
-
 		<center><br>
 			<h1>Control De Notas Del Estudiante</h1>
+
+					<table class="table">
+						<th><center>Nombre Del Estudiante</center></th>
+						<th><center>Carrera</center></th>
+						<th><center>Semestre</center></th>
+						<th><center>Nombre Del Curso</center></th>
+						<th><center>Primer Parcial</center></th>
+						<th><center>Segundo Parcial</center></th>
+						<th><center>Tareas</center></th>
+						<th><center>Parcial Final</center></th>
+
+						<?php
+
+						$busqueda = "SELECT * FROM asignatura WHERE idProfesor=$idProfesor";
+						$busque = $mysqli->query($busqueda) or die (mysql_error($mysqli));
+						while($fila=$busque->fetch_assoc())
+						{
+							$idAsignatur = mysqli_real_escape_string($mysqli,$fila['idCurso']);//LISTO
+							$idCurso = mysqli_real_escape_string($mysqli,$fila['idCurso']);//LISTO
+							$PrimerParcial = mysqli_real_escape_string($mysqli,$fila['PrimerPar']);//LISTO
+							$SegundoParcial = mysqli_real_escape_string($mysqli,$fila['SegundoPar']);//LISTO
+							$Tareas = mysqli_real_escape_string($mysqli,$fila['Tareas']);//LISTO
+							$ParcialFinal = mysqli_real_escape_string($mysqli,$fila['ParcialFinal']);//LISTO
+							$idAlumno = mysqli_real_escape_string($mysqli,$fila['idAlumno']);//LISTO
+							$idCarrera = mysqli_real_escape_string($mysqli,$fila['idCarrera']);
+							$idSemestre = mysqli_real_escape_string($mysqli,$fila['idSemestre']);//LISTO
+
+			        $query1 = "SELECT Nombre FROM curso WHERE idCurso=$idCurso";
+			        $consulta1 =$mysqli->query($query1) or die (mysql_error($mysqli));
+			        $fila1 = $consulta1->fetch_assoc();
+			        $NombreCurso =$fila1['Nombre'];//LISTO
+
+			        $query2 = "SELECT NoSemestre FROM semestre WHERE idSemestre=$idSemestre";
+			        $consulta2 = $mysqli->query($query2) or die (mysql_error($mysqli));
+			        $fila2 = $consulta2->fetch_assoc();
+			        $NoSemestre = $fila2['NoSemestre'];//LISTO
+
+							$query3 = "SELECT Nombre FROM alumnos WHERE idAlumno=$idAlumno";
+			        $consulta3 = $mysqli->query($query3) or die (mysql_error($mysqli));
+			        $fila3 = $consulta3->fetch_assoc();
+			        $NombreAlumno = $fila3['Nombre'];//LISTO
+
+							$query4 = "SELECT NombreCarrera FROM carrera WHERE idCarrera=$idCarrera";
+							$consulta4 = $mysqli->query($query4) or die (mysql_error($mysqli));
+							$fila4 = $consulta4->fetch_assoc();
+							$NombreCarrera = $fila4['NombreCarrera'];
+							echo "<tr>";
+								echo "<td><center>"; echo $NombreAlumno; echo "</center></td>";
+								echo "<td><center>"; echo $NombreCarrera; echo " </center></td>";
+								echo "<td><center>"; echo $NoSemestre; echo "</center></td>";
+								echo "<td><center>"; echo $NombreCurso; echo " </center></td>";
+								echo "<td><center>"; echo $PrimerParcial; echo "</center></td>";
+								echo "<td><center>"; echo $SegundoParcial; echo " </center></td>";
+								echo "<td><center>"; echo $Tareas; echo "</center></td>";
+								echo "<td><center>"; echo $ParcialFinal; echo " </center></td>";
+
+			          echo "<td><a href='modifynotes.php?numero=".$idAsignatur."'><button type='button' name='INGRESAR NOTAS' class='btn btn-danger'>MODIFICAR PUNTEOS</Button></a></td>";
+								echo "<td><a href='#?numero=".$idAsignatur."'><button type='button' name='IMPRIMIR' class='btn btn-success'>IMPRIMIR</Button></a></td>";
+							echo "<tr>";
+						}?>
+						</table>
+						<?php
+						 echo "<a href='index.php?><button type='button' class='btn btn-dark col-md-3'>VOLVER</Button></a>"
+						?>
+					</center>
+
+					<script src="../../js/bootstrap.js"></script>
+				<script src="../../js/jquery-3.2.1.min.js"></script>
+				<script src="../../js/bootstrap.min.js"></script>
 		</center>
 	</body>
-
 </html>
-
-
