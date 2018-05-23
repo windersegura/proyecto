3ae4020cf2aa165886c9afb56f3aa53c696b18ff
windersegura <windersegura@hotmail.com>
2018-05-23 01:47:22 -0600

    cambios

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Amatic+SC|Didact+Gothic|Roboto">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-	<link rel="stylesheet" href="../../css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
@@ -73,7 +73,7 @@
 							<li>
 								<img src="img(1)/universitarios1.jpg" alt="" width="500">
 								<div class="caption">
-										<h3 class="caption-title"></h3>
+										<h3 class="caption-title">Conoce Nuevas Personas</h3>
 										<p></p>
 
 								</div>
@@ -83,7 +83,7 @@
 							<li>
 								<img src="img(1)/profecionales.jpg" alt="" width="500">
 								<div class="caption">
-										<h3 class="caption-title"></h3>
+										<h3 class="caption-title">Conviertete en un Profesional</h3>
 										<p></p>
 
 								</div>
@@ -92,7 +92,7 @@
 							<li>
 								<img src="img(1)/superacion.jpg" alt="" width="500">
 								<div class="caption">
-										<h3 class"caption-title"></h3>
+										<h3 class"caption-title">Alcanza tus Sueños </h3>
 										<p></p>
 
 								</div>
