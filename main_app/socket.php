7b9562b3bbcabcf8aaf1f29dfecffbf0806b885a
windersegura <windersegura@hotmail.com>
2018-05-22 22:47:31 -0600

    socket

--- main_app/socket.php
+++ main_app/socket.php
@@ -17,4 +17,33 @@ echo socket_strerror(socket_last_error());
 socket_listen($socket);
 
 
+while(1){
+    // aceptamos la conexion que nos entre
+    $cliente[++$i] = socket_accept($socket);
+    // leemos la informacion que nos envian
+    $input = socket_read($cliente[$i], 1024);
+    // quitamos espacios y saltos de linea de lo que se lee
+    $ticker = ereg_replace("[ \t\r\n]", "", $input);
+    // escribimos lo que recibimos
+    echo "Ticker: $ticker";
+    
+    if(array_key_exists($ticker, $contenido)){
+        // ahora si buscamos la informacion que leimos en el socket
+        // dentro del array de contenido
+        $datos = $contenido[$ticker];
+    }else{
+        // si no existe pues le decimos que lo que
+        // busco no esta dentro del contenido
+        $datos = "No se encontrontraron los datos";
+    }
+    // escribimos los resultados que encontramos dentro del
+    // array en el socket para que el cliente los lea
+    socket_write($cliente[$i], $datos . "\n\r", 1024);
+    // cerramos la conexion de ese cliente
+    socket_close($cliente[$i]);
+}
+// cerramos la conexion global
+socket_close($socket);
+
+
 ?>
