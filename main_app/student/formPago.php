86e225f8c105667ef3e2d298b6028867cb1c432b
windersegura <windersegura@hotmail.com>
2018-05-28 23:46:53 -0600

    cambios

--- main_app/student/formPago.php
+++ main_app/student/formPago.php
@@ -49,7 +49,7 @@
               <div class="col-md-4">
 
                 <label>Numero de Tarjeta <span><em>(requerido)</em></span></label><br>
-            <input type="text" pattern="[0-9]{13}" name="Tarjeta" class="form-input form-control" placeholder="Ingrese su Numero de Tarjeta" required/>
+            <input type="text" minlength="1" maxlength="6" name="Tarjeta" class="form-input form-control" placeholder="Ingrese su Numero de Tarjeta" required/>
 
               </div>
 
@@ -75,51 +75,7 @@
 
             </form>
 
-<?php 
 
-date_default_timezone_set('UTC');
-
-//define(banco,"00");
-//define(institucion,"0001");
-$banco="00";
-$institucion="0001";
-$fecha=date('dmy');
-$hora= date('gis');
-$trx="01";
-$trama="";
-
-
-$estado="0000";
-
-$cod_referencia="0000";
-
-
-$port='9999';
-$addres='192.168.43.157';
-$i=0;
-
-while ($i<1) {
-  # code...
-
- $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("No se pudo conectar ");
- $result= socket_connect($socket, $addres, $port) or die("No se pudo conectar");
-  $msg="";
-  $msg="". $banco . $trx . $fecha . $hora . $_REQUEST['Tarjeta'] . $_REQUEST['Seguridad'] . $institucion . $_REQUEST['Monto'] . $estado . $cod_referencia;
-
-$trama=socket_write($socket, $msg, strlen($msg)) or die("No se pudo enviar el mensaje al servidor");
-
-$result= socket_read($socket, 1024) or die("No hay respuesta del servidor");
-
-
-
-socket_close($socket);
-
-
-$i++;
-
-}
-
-?>
 
 
         </div>
@@ -132,49 +88,4 @@ $i++;
 </html>
 
 
-<?php 
-/*
-date_default_timezone_set('UTC');
-
-//define(banco,"00");
-//define(institucion,"0001");
-$banco="00";
-$institucion="0001";
-$fecha=date('dmy');
-$hora= date('gis');
-$trx="01";
-$trama="";
-
-
-$estado="0000";
-
-$cod_referencia="0000";
-
-
-$port='81';
-$addres='192.168.43.6';
-$i=0;
-
-while ($i<1) {
-  # code...
-
- $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("No se pudo conectar ");
- $result= socket_connect($socket, $addres, $port) or die("No se pudo conectar");
-  $msg="";
-  $msg="". $banco . $trx . $fecha . $hora . $_REQUEST['Tarjeta'] . $_REQUEST['Seguridad'] . $institucion . $_REQUEST['Monto'] . $estado . $cod_referencia;
-
-$trama=socket_write($socket, $msg, strlen($msg)) or die("No se pudo enviar el mensaje al servidor");
-
-$result= socket_read($socket, 1024) or die("No hay respuesta del servidor");
-
-
-
-socket_close($socket);
-
-
-$i++;
-
-}
-*/
-?>
 
---
+++ main_app/student/recibe_form.php
@@ -0,0 +1,45 @@
+<?php 
+
+date_default_timezone_set('UTC');
+
+//define(banco,"00");
+//define(institucion,"0001");
+$banco="00";
+$institucion="0001";
+$fecha=date('dmy');
+$hora= date('gis');
+$trx="01";
+$trama="";
+
+
+$estado="0000";
+
+$cod_referencia="0000";
+
+
+$port='9999';
+$addres='192.168.43.157';
+$i=0;
+
+while ($i<1) {
+  # code...
+
+ $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("No se pudo conectar ");
+ $result= socket_connect($socket, $addres, $port) or die("No se pudo conectar");
+  $msg="";
+  $msg="". $banco . $trx . $fecha . $hora . $_REQUEST['Tarjeta'] . $_REQUEST['Seguridad'] . $institucion . $_REQUEST['Monto'] . $estado . $cod_referencia;
+
+$trama=socket_write($socket, $msg, strlen($msg)) or die("No se pudo enviar el mensaje al servidor");
+
+$result= socket_read($socket, 1024) or die("No hay respuesta del servidor");
+
+
+
+socket_close($socket);
+
+
+$i++;
+
+}
+
+?>
